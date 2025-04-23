a7cc78c0df109134aebd2382906526c253e85865
Anik Shakya <aniklinkin@gmail.com>
2025-04-23 18:41:09 +0545

    Modified

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Vimeo Uploader and Player',
+      
       theme: ThemeData(
         primarySwatch: Colors.red,
         scaffoldBackgroundColor: Colors.black,
